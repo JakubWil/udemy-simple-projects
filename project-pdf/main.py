3f10a1f0a40099e7f2fc58b63cefb954889d8137
Jakub Wilczek <jacob_0909@icloud.com>
2026-02-26 13:30:42 +0100

    Lined PDF

--- project-pdf/main.py
+++ project-pdf/main.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 
 pdf = FPDF(orientation='P', unit='mm', format='A4')
+pdf.set_auto_page_break(auto=False, margin=0)
 df = pd.read_csv('data.csv')
 
 
@@ -13,7 +14,39 @@ for index, row in df.iterrows():
     pdf.cell(0, 12, txt=row['Topic'], ln=1, align='L')
     pdf.line(10, 20, 200, 20)
 
+    for line in range(20,280, 10):
+       
+         pdf.line(10, line, 200, line)
+
     
+    pdf.ln(265)
+    
+
+    #Set footer for main page
+    pdf.set_font('Times', 'I', 8)
+    pdf.set_text_color(180,180,180)
+    pdf.cell(0, 10, txt=row['Topic'], ln=1, align='R')
+
+
+    for n in range(int(row['Pages']) - 1):
+        
+
+
+        pdf.add_page()
+        pdf.ln(277)
+
+        for line in range(20,280, 10):
+           
+            pdf.line(10, line, 200, line)
+        
+
+         #Set footer for other pages
+        pdf.set_font('Times', 'I', 8)
+        pdf.set_text_color(180,180,180)
+        pdf.cell(0, 10, txt=row['Topic'], ln=1, align='R')
+       
+
+
   
 
 
